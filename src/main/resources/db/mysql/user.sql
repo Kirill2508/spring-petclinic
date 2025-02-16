0074b324c2f35cac902733fcee3c8f12ad871f00
Kirill <fessohfess@gmail.com>
2025-02-16 11:08:47 +0100

    Update user.sql

--- src/main/resources/db/mysql/user.sql
+++ src/main/resources/db/mysql/user.sql
@@ -4,4 +4,10 @@ ALTER DATABASE petclinic
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
-GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
+-- 1) Create or alter the user with a password
+CREATE USER IF NOT EXISTS 'petclinic'@'%' IDENTIFIED BY 'petclinic';
+
+-- 2) Grant privileges on the petclinic DB
+GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%';
+
+FLUSH PRIVILEGES;
